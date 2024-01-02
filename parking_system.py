c131c2128d50609957309b2f9fcec46675879912
TommyA8 <nengnoia8@gmail.com>
2024-01-03 01:19:10 +0700

    Solve bug

--- parking_system.py
+++ parking_system.py
@@ -6,28 +6,31 @@ import os
 from collections import defaultdict
 from datetime import datetime
 from pandas import DataFrame
+import time
 
-class Detection_model():
+class Detection_model(object):
     def __init__ (self, model_path:str):
         self.model = YOLO(model_path)
-        self.model.to('cuda' if torch.cuda.is_available() else 'cpu') 
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print(f"Using Device: {self.device}")
 
     def detect(self, frame):
         '''
         return  list of x, y, w, h, score, class_ids and 
                 dictionary of class names {0: 'empty', 1: 'occupied'}
         '''
-        results = self.model(frame, verbose=False)[0]
+        results = self.model(frame, verbose=False, iou=0.5)[0]
         return results.boxes.data.tolist(), results.names # names: {0: 'empty', 1: 'occupied'}
 
-class ParkingManager():
-    def __init__(self, detector:object, 
-                 tracker:object, 
-                 video_path=None,
-                 threshold:float= 0.3,
-                 resize:tuple=None,
-                 save_video:bool=False,
-                 save_table:bool=False):
+class ParkingManager(object):
+    def __init__(self, 
+                detector:object, 
+                tracker:object, 
+                video_path=None,
+                threshold:float= 0.3,
+                resize:tuple=None,
+                save_video:bool=False,
+                save_table:bool=False):
         
         self.detector = detector
         self.tracker = tracker
@@ -40,7 +43,7 @@ class ParkingManager():
 
     def export_video(self, size:tuple=None, fps:int=30):
         assert size, "Required output video size"
-        output_path = './outputs/OUTPUT.mp4'
+        output_path = './outputs/OUTPUT_VIDEO.mp4'
         return cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, size)
 
     def plot_boxes(self, frame, bbox, class_name=None, track_id=None):
@@ -62,7 +65,7 @@ class ParkingManager():
     def run(self):
 
         if self.save_table:
-            parking_df = DataFrame(columns=['ID', 'Parked_at', 'Left_at', 'Duration'])
+            parking_df = DataFrame(columns=['Car_ID', 'Parked_at', 'Left_at', 'Duration'])
 
         if self.save_video:
             if self.resize:
@@ -74,7 +77,9 @@ class ParkingManager():
 
         track_history = defaultdict(lambda:[])
 
-        while 1:
+        while self.cap.isOpened():
+            start = time.perf_counter()
+
             ret, frame = self.cap.read()
             if not ret:
                 break
@@ -97,14 +102,15 @@ class ParkingManager():
                 
             tracks = self.tracker.update_tracks(occupiedDetected, frame=frame)
 
-            if track_history:
-                removed_IDs = [track_history.pop(id) for id in track_history if id not in [track.track_id for track in tracks]]
-                if self.save_table:
-                    for id in removed_IDs:
-                        parked_at = id['Parked_at']
-                        left_at = datetime.now()
-                        duration = left_at - parked_at
-                        parking_df.loc[len(parking_df)] = [id, parked_at, left_at, duration]
+            # check if any car left
+            leaving_IDs = [(id, track_history.pop(id)) for id in list(track_history) if id not in [track.track_id for track in tracks]]
+            # print(leaving_IDs)
+            if self.save_table:
+                for id, parked_info in leaving_IDs:
+                    parked_at = parked_info[0]['Parked_at']
+                    left_at = datetime.now()
+                    duration = left_at - parked_at
+                    parking_df.loc[len(parking_df)] = [id, parked_at, left_at, duration]
                                  
             for track in tracks:
                 if not track.is_confirmed():
@@ -112,16 +118,21 @@ class ParkingManager():
                 ltrb = track.to_ltrb(orig=True)
                 track_id = track.track_id
                 # store tracking id
-                if track_id in track_history:
-                    track = track_history[track_id]
-                    track.append({'Parked_at': datetime.now()})
+                if track_id not in track_history:
+                    track_history[track_id].append({'Parked_at': datetime.now()})
                 # plotting
                 self.plot_boxes(frame, ltrb, track_id=track_id)
-
-            cv2.imshow("Parking System", frame)
-
+            #print("track_history",track_history.keys())
+            
             if self.save_video:
                 cap_out.write(frame)
+
+            end = time.perf_counter()
+            total =  end - start
+            fps = 1/total
+            cv2.putText(frame, f"FPS:{fps}", (10,30), cv2.FONT_HERSHEY_DUPLEX, 0.8, (0,0,0), 2)
+
+            cv2.imshow("Parking System", frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -138,7 +149,7 @@ if __name__ == "__main__":
     import timeit
 
     model_path = "./detection_model/train3/weights/best.pt"
-    video_path = "./videos/test.mp4"
+    video_path = "./videos/test.mp4" #BLK-HDPTZ12 Security Camera Parkng Lot Surveillance Video.mp4
     
     usage = ParkingManager( detector=Detection_model(model_path=model_path),
                             tracker=DeepSort(),
@@ -149,4 +160,4 @@ if __name__ == "__main__":
                             save_table=True )
     #usage.run()
 
-    print("Benchmark", timeit.timeit(lambda: usage.run(), setup="pass",number=1))
+    print("Benchmark", (timeit.timeit(lambda: usage.run(), setup="pass",number=1))/60, "min")
